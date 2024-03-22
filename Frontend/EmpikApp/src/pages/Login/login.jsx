8043fe3c52c9530850909cc3f4ade288ae54851e
Kamil Sobczyk <s092803@student.tu.kielce.pl>
2024-03-22 23:55:17 +0100

    Dodanie panelu logowania,rejestracji i routingu

---
+++ Frontend/EmpikApp/src/pages/Login/login.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Login() {
+  return (
+    <div className="body_login"> 
+      <h2>Login</h2>
+      <input type="text" placeholder="Username" />
+      <input type="password" placeholder="Password"/>
+      <button>Login</button>
+    </div>
+  );
+}
+export default Login;
---
+++ Frontend/EmpikApp/src/pages/Registration/registration.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Registration() {
+  return (
+    <div className="body_registatration"> 
+      <h2>Login</h2>
+      <input type="text" placeholder="Username" />
+      <input type="password" placeholder="Password"/>
+      <button>Rejestruj</button>
+    </div>
+  );
+}
+export default Registration;
