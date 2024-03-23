22c3ee96a26317049c07a5d44e6a73855b92b67a
BaKernelsh <bartosz.klnr506@gmail.com>
2024-03-23 19:22:49 +0100

    CRUD produkty

---
+++ Backend/src/main/java/org/example/empikserver/controller/ProductController.java
@@ -0,0 +1,126 @@
+package org.example.empikserver.controller;
+
+import org.example.empikserver.model.Product;
+import org.example.empikserver.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@CrossOrigin(origins="*")
+@RestController
+@RequestMapping("/api")
+public class ProductController {
+
+    @Autowired
+    ProductRepository productRepository;
+
+    @GetMapping("/products")
+    public ResponseEntity<List<Product>> getAllProducts(@RequestParam(required=false) String name) {
+        try{
+            List<Product> products = new ArrayList<>();
+
+            if(name==null){
+                products.addAll(productRepository.findAll());
+            }
+            else{
+                products.addAll(productRepository.findByNameContaining(name));
+            }
+
+            if(products.isEmpty()){
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+
+            return new ResponseEntity<>(products, HttpStatus.OK);
+        } catch( Exception e){
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/products/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable("id") long id) {
+
+            Optional<Product> product = productRepository.findById(id);
+
+            if(product.isPresent()) {
+                return new ResponseEntity<>(product.get(),HttpStatus.OK);
+            }
+            else{
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+    }
+
+    @PostMapping("/products")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<Product> createProduct(@RequestBody Product product) {
+        try{
+            Product prod = productRepository
+                    .save(new Product(product, false));
+            return new ResponseEntity<>(prod, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping("/products/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<Product> updateProduct(@PathVariable("id") long id,
+             @RequestBody Product product) {
+
+        Optional<Product> prod = productRepository.findById(id);
+
+        if(prod.isPresent()){
+            Product updatedProduct = prod.get();
+            updatedProduct.setName(product.getName());
+            updatedProduct.setPrice(product.getPrice());
+            updatedProduct.setProducer(product.getProducer());
+            updatedProduct.setDescription(product.getDescription());
+            updatedProduct.setRecommendedAge(product.getRecommendedAge());
+            updatedProduct.setRecommendedSex(product.getRecommendedSex());
+            updatedProduct.setMaterial(product.getMaterial());
+            updatedProduct.setHeightInMilimeters(product.getHeightInMilimeters());
+            updatedProduct.setWidthInMilimeters(product.getWidthInMilimeters());
+            updatedProduct.setDepthInMilimeters(product.getDepthInMilimeters());
+            updatedProduct.setWeightInGrams(product.getWeightInGrams());
+            updatedProduct.setAvailable(product.isAvailable());
+
+            return new ResponseEntity<>(productRepository.save(updatedProduct), HttpStatus.OK);
+
+        }else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+    }
+
+    @DeleteMapping("/products/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<HttpStatus> deleteProduct(@PathVariable("id") long id) {
+        try{
+            productRepository.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
+    @DeleteMapping("/products")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<HttpStatus> deleteAllProducts() {
+        try {
+            productRepository.deleteAll();
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
+
+
+}
---
+++ Backend/src/main/java/org/example/empikserver/model/Product.java
@@ -0,0 +1,191 @@
+package org.example.empikserver.model;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+
+@Entity
+@Table(name="products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column(scale=2)
+    private BigDecimal price;
+
+    @Column
+    private String producer;
+
+    @Column
+    private String description;
+
+    @Column
+    private int recommendedAge;
+
+    @Column
+    private String recommendedSex;
+
+    @Column
+    private String material;
+
+    @Column
+    private Integer heightInMilimeters;
+
+    @Column
+    private Integer widthInMilimeters;
+
+    @Column
+    private Integer depthInMilimeters;
+
+    @Column
+    private Integer weightInGrams;
+
+    @Column
+    private boolean isAvailable;
+
+    public Product(){
+
+    }
+
+    public Product(String name, BigDecimal price, String producer, String description, int recommendedAge, String recommendedSex, String material,
+                   Integer heightInMilimeters, Integer widthInMilimeters, Integer depthInMilimeters, Integer weightInGrams, boolean isAvailable) {
+        this.name = name;
+        this.price = price;
+        this.producer = producer;
+        this.description = description;
+        this.recommendedAge = recommendedAge;
+        this.recommendedSex = recommendedSex;
+        this.material = material;
+        this.heightInMilimeters = heightInMilimeters;
+        this.widthInMilimeters = widthInMilimeters;
+        this.depthInMilimeters = depthInMilimeters;
+        this.weightInGrams = weightInGrams;
+        this.isAvailable = isAvailable;
+    }
+
+    public Product(Product product, boolean isAvailable){
+        this.name = product.getName();
+        this.price = product.getPrice();
+        this.producer = product.getProducer();
+        this.description = product.getDescription();
+        this.recommendedAge = product.getRecommendedAge();
+        this.recommendedSex = product.getRecommendedSex();
+        this.material = product.getMaterial();
+        this.heightInMilimeters = product.getHeightInMilimeters();
+        this.widthInMilimeters = product.getWidthInMilimeters();
+        this.depthInMilimeters = product.getDepthInMilimeters();
+        this.weightInGrams = product.getWeightInGrams();
+        this.isAvailable = isAvailable;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getProducer() {
+        return producer;
+    }
+
+    public void setProducer(String producer) {
+        this.producer = producer;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getRecommendedAge() {
+        return recommendedAge;
+    }
+
+    public void setRecommendedAge(int recommendedAge) {
+        this.recommendedAge = recommendedAge;
+    }
+
+    public String getRecommendedSex() {
+        return recommendedSex;
+    }
+
+    public void setRecommendedSex(String recommendedSex) {
+        this.recommendedSex = recommendedSex;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    public Integer getHeightInMilimeters() {
+        return heightInMilimeters;
+    }
+
+    public void setHeightInMilimeters(Integer heightInMilimeters) {
+        this.heightInMilimeters = heightInMilimeters;
+    }
+
+    public Integer getWidthInMilimeters() {
+        return widthInMilimeters;
+    }
+
+    public void setWidthInMilimeters(Integer widthInMilimeters) {
+        this.widthInMilimeters = widthInMilimeters;
+    }
+
+    public Integer getDepthInMilimeters() {
+        return depthInMilimeters;
+    }
+
+    public void setDepthInMilimeters(Integer depthInMilimeters) {
+        this.depthInMilimeters = depthInMilimeters;
+    }
+
+    public Integer getWeightInGrams() {
+        return weightInGrams;
+    }
+
+    public void setWeightInGrams(Integer weightInGrams) {
+        this.weightInGrams = weightInGrams;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
+}
