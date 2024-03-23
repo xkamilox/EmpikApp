3a65c1a294ca826e7317397dfe82d87d4ead4716
monika jedynak <monikajedynak@interia.pl>
2024-03-23 16:20:53 +0100

    add spring to configurate #TODO sprawdzic aby tez byl jakis user w springu i testowac endpoint bez logowania sie

--- Backend/src/main/java/org/example/empikserver/security/WebSecurityConfig.java
+++ Backend/src/main/java/org/example/empikserver/security/WebSecurityConfig.java
@@ -58,6 +58,7 @@ public class WebSecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth ->
 
+
                         auth.requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()//#TODO sprawdzic aby tez byl jakis user w springu i testowac endpoint bez logowania sie
                         .requestMatchers("/api/auth/**").permitAll()
                                 .requestMatchers("/api/test/**").permitAll()
