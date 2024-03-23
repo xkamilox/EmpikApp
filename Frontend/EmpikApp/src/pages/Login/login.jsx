0f5b14778d5508ccbc95aaf740491132938c5746
Kamil Sobczyk <s092803@student.tu.kielce.pl>
2024-03-23 16:57:55 +0100

    Dodanie styli w panelu logowania

--- Frontend/EmpikApp/src/pages/Login/login.jsx
+++ Frontend/EmpikApp/src/pages/Login/login.jsx
@@ -1,12 +1,32 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+import PATH from "../../paths";
 
 function Login() {
   return (
     <div className="body_login"> 
-      <h2>Login</h2>
-      <input type="text" placeholder="Username" />
-      <input type="password" placeholder="Password"/>
-      <button>Login</button>
+      <div className="login_container">
+        <h2>Login</h2>
+        <input type="text" placeholder="Email" className="input_field"/>
+        <input type="password" placeholder="Password" className="input_field"/>
+        <div className="button_container">
+          <div className='register_conatiner'>
+            <text>Don't have an account?</text>
+            <Link to={PATH.REGISTRATION}>
+              <button className='button_register'>Create new</button>
+            </Link>
+          </div>
+          <button className='button'>Login</button>
+        </div>
+        <div className='text_container'>
+          <text className='text2'>Or Login With</text>
+        </div>
+        <div className='google_container'>
+          <button className='button_google'>
+            <img src="/src/images/Login/google.png" alt="Button Image" />Sign in with Google
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
