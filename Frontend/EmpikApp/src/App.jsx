2b0b27583c1c9ca18ec71443066078ace6c0594d
Kamil Sobczyk <s092803@student.tu.kielce.pl>
2024-05-04 16:37:25 +0200

    Add:Model product and shopping cart

--- Frontend/EmpikApp/src/App.jsx
+++ Frontend/EmpikApp/src/App.jsx
@@ -4,12 +4,16 @@ import PATH from './paths';
 import './App.css';
 import Login from './pages/Login/login';
 import Registration from './pages/Registration/registration';
+import Product from './pages/Product/product';
+import Shopping_cart from './pages/Shopping_cart/shopping_cart';
 
 function App() {
   return (
     <Routes>
       <Route index path={PATH.LOGIN} element={<Login/>}/>
       <Route path={PATH.REGISTRATION} element={<Registration/>}/>
+      <Route path={PATH.PRODUCT} element={<Product/>}/>
+      <Route path={PATH.SHOPPING_CART} element={<Shopping_cart/>}/>
       <Route path="*" element={<Navigate to={PATH.LOGIN} replace />} />
     </Routes>
   )
