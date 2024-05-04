c0872dc70839e0d9dfe7da1ad2ed78090ff218de
BaKernelsh <bartosz.klnr506@gmail.com>
2024-05-04 22:14:05 +0200

    logowanie i rejestracja przez frontend

---
+++ Backend/src/main/java/org/example/empikserver/repository/RefreshTokenRepository.java
@@ -0,0 +1,17 @@
+package org.example.empikserver.repository;
+
+import org.example.empikserver.model.RefreshToken;
+import org.example.empikserver.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByToken(String token);
+
+    @Modifying
+    int deleteByUser(User user);
+}
